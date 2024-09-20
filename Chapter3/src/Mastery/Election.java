930bbc7eb29def54d53f8261679e8699827ad141
38203515 <38203515@L815WD220015.edu.cbe.ab.ca>
2024-09-20 10:46:31 -0600

    10:46 A.M.

--- Chapter3/src/Mastery/Election.java
+++ Chapter3/src/Mastery/Election.java
@@ -1,5 +1,6 @@
 package Mastery;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Election {
@@ -9,50 +10,58 @@ public class Election {
 		Scanner userInput = new Scanner(System.in);
 		
 		
-		System.out.print("Please enter the election results for the following candidates in New York:");
+		System.out.println("\nPlease enter the election results for the following candidates in New York:");
      
-		System.out.print("\nAwbrey: ");
-		int Awbreyny = userInput.nextInt();
+		System.out.println("\nAwbrey: ");
+		double Awbreyny = userInput.nextInt();
 		
-		System.out.print("\nMartinez: ");
-		int Martinezny = userInput.nextInt();
+		System.out.println("Martinez: ");
+		double Martinezny = userInput.nextInt();
 		
 		
 		
-		System.out.print("Please enter the election results for the following candidates in New Jersey:");
+		System.out.print("\nPlease enter the election results for the following candidates in New Jersey: ");
 	     
-		System.out.print("\nAwbrey: ");
-		int Awbreynj = userInput.nextInt();
+		System.out.println("Awbrey: ");
+		double Awbreynj = userInput.nextInt();
 		
-		System.out.print("\nMartinez: ");
-		int Martineznj = userInput.nextInt();
-		
-		
+		System.out.println("Martinez: ");
+		double Martineznj = userInput.nextInt();
 		
 		
-		System.out.print("Please enter the election results for the following candidates in Connecticut:");
+		
+		
+		System.out.print("\nPlease enter the election results for the following candidates in Connecticut:");
 	     
-		System.out.print("\nAwbrey: ");
-		int Awbreyc = userInput.nextInt();
+		System.out.println("Awbrey: ");
+		double Awbreyc = userInput.nextInt();
 		
-		System.out.print("\nMartinez: ");
-		int Martinezc = userInput.nextInt();
+		System.out.println("Martinez: ");
+		double Martinezc = userInput.nextInt();
 		
-		int Martineztotal = Martinezc + Martineznj + Martinezny;
 		
-		int Awbreytotal = Awbreyc + Awbreynj + Awbreyny;
 		
-		int totalvotes = Martineztotal + Awbreytotal;
 		
-		int Awbreypercent = (Awbreytotal*100)/totalvotes;
 		
-		int Martinezpercent = (Martineztotal*100)/totalvotes;
+		double Martineztotal = Martinezc + Martineznj + Martinezny;
 		
-		System.out.printf("Awbrey's total votes were: " + Awbreytotal);
-		System.out.printf("\nAwbrey had" + Awbreypercent + "% of the votes");
+		double Awbreytotal = Awbreyc + Awbreynj + Awbreyny;
 		
-		System.out.printf("\n\nMartinez's total votes were: " + Martineztotal);
-		System.out.printf("\nMartinez had" + Martinezpercent + "% of the votes");
+		double totalvotes = Martineztotal + Awbreytotal;
+		
+		double Awbreypercent = (Awbreytotal*100)/totalvotes;
+		
+		double Martinezpercent = (Martineztotal*100)/totalvotes;
+		
+		 DecimalFormat Awbreypercentf = new DecimalFormat("#.00");
+
+		 DecimalFormat Martinezpercentf = new DecimalFormat("#.00");
+		
+		System.out.print("Awbrey's total votes were: " + Awbreytotal);
+		System.out.print("\nAwbrey's percent of the votes were " + (Awbreypercentf.format(Awbreypercent)) + "%");
+		
+		System.out.print("\n\nMartinez's total votes were: " + Martineztotal);
+		System.out.print("\nMartinez's percent of the vots were " + (Martinezpercentf.format(Martinezpercent)) + "%");
 		
 	}
 		
