957a8b0c262c0633c5ab1626091581a28d386c73
38203515 <38203515@L815WD220015.edu.cbe.ab.ca>
2024-10-02 11:58:56 -0600

    11:58 A.M.

---
+++ Chapter5/src/Skillbuilders/Evens.java
@@ -0,0 +1,23 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class Evens {
+
+	public static void main(String[] args)
+	{
+		
+
+	
+	System.out.print("The even numbers are ");
+	
+	for (int evennumbers = 0; evennumbers <= 20; evennumbers += 2) {
+		System.out.print(evennumbers + "   ");	}
+	
+	
+	
+	
+
+}
+
+}
---
+++ Chapter5/src/Skillbuilders/Numberssum.java
@@ -0,0 +1,32 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class Numberssum {
+	
+
+	public static void main(String[] args)
+	{
+	
+	Scanner input = new Scanner(System.in);
+	
+	System.out.print("Enter a number: ");
+	int num = input.nextInt();
+	
+	System.out.print("The integers 1 to your number are: ");
+	
+	for (int numbers = 1; numbers <= num; numbers += 1) {
+		System.out.print(numbers + "   ");	}
+	
+	int sum += 1
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+}
