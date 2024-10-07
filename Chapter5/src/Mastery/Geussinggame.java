94727f35bb6629b52b7fd90098fce4cd8dbd359d
38203515 <38203515@L815WD220015.edu.cbe.ab.ca>
2024-10-07 11:53:51 -0600

    11:53 A.M.

--- Chapter5/src/Mastery/Geussinggame.java
+++ Chapter5/src/Mastery/Geussinggame.java
@@ -1,5 +1,31 @@
 package Mastery;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Geussinggame {
 
-}
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+
+            int randomnum = (int)(20 * Math.random() + 1);
+            System.out.println("Enter a number between 1 and 20: ");
+
+            int answer = 0; // Initialize the user's guess, talk about how you originally didn't have this here
+
+            // Loop until the user guesses the correct number
+            while (answer != randomnum) {
+                answer = input.nextInt(); // write how you had this outside the loop first and it didn't work
+
+                if (answer == randomnum) {
+                    System.out.println("Computer's Number: " + randomnum);
+                    System.out.println("Player's Number: " + answer);
+                    System.out.println("You won!");
+                } else {
+                    System.out.println("Player's Number: " + answer);
+                    System.out.println("Try again: ");
+                }
+            }
+
+        }
+    }
--- Chapter5/src/Mastery/Primenumbers.java
+++ Chapter5/src/Mastery/Primenumbers.java
@@ -1,29 +1,31 @@
 package Mastery;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Primenumbers {
 
-    public static void main(String[] args) {
-	    	
-    	Scanner input = new Scanner(System.in);
-    	
-    	System.out.print("Enter a number: ");
-    	int userinput = input.nextInt();
+  public static void main(String[] args) {
+   
+    Scanner input = new Scanner(System.in);
 
-    	int prime = 2;
-    	
-	    	
-	    	
-	    	
-	    	for (int division = 2; !(userinput%division == 0); division += 1) {
-	    		System.out.print(userinput + " is not a prime number.");	}
-	   
-	    	
-	    	
-	    	
-	    	
-	    	
-	        }
-	    }
+    // Prompt the user to enter a number
+    System.out.print("Enter a number you want to know is prime or not: ");
+    int number = input.nextInt();
+
+    // Assume the number is prime unless proven otherwise
+    boolean isPrime = true;
+
+    // Check divisability for numbers from 2 to number-1
+    for (int i = 2; i < number && isPrime; i++) {
+      // Use modulo operator to check if there is any divisor
+      isPrime = (number % i != 0);
+    }
+
+    // Output the result based on the isPrime flag ? is like an if else-statement
+    System.out.println(number + (isPrime ? " is a prime number." : " is not a prime number."));
+
+  
+  }
+}
 
